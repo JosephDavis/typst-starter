ecc01a72fb44d929510566e80ba078a71c647eb7
Joey Davis <JosephDavis@users.noreply.github.com>
2025-02-17 11:04:04 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -6,21 +6,26 @@ if [ "$#" -eq 1 ]; then
 
     # Check if the file exists and has a .typ extension
     if [ -f "$file" ] && [[ "$file" == *.typ ]]; then
-        out_file="${file%%.*}"
-        out_file="${out_file//docs/out}.pdf" 
+        echo "Compiling Typst file: $file"
+        # Extract filename with extension
+        filename_with_ext=$(basename "$file")
+        out_file="${filename_with_ext%.*}"
+        out_file="./compiled/${out_file}.pdf" 
 
         mkdir -p "$(dirname "$out_file")"
-        typst compile "$file" "$out_file"
+        typst compile "$file" "$out_file" --format pdf --root .
     else
         echo "Error: File not found or not a .typ file."
     fi
 else
     # If no argument is provided, compile all Typst files recursively
     find docs -name "*.typ" | while read file; do
-        out_file="${file%%.*}"
-        out_file="${out_file//docs/out}.pdf" 
+        # Extract filename with extension
+        filename_with_ext=$(basename "$file")
+        out_file="${filename_with_ext%.*}"
+        out_file="./compiled/${out_file}.pdf"
 
         mkdir -p "$(dirname "$out_file")"
-        typst compile "$file" "$out_file" 
+        typst compile "$file" "$out_file" --format pdf --root .
     done
 fi
